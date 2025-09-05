6bb96aa248522a79034f82d644c6b65d4aab2687
ty2arjit <ty2arjit@gmail.com>
2025-09-05 20:56:56 +0530

    If user is not logged in then 2 columns otherwise 3 columns

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import RightSidebar from '../components/RightSidebar' 
+import ChatContainer from '../components/ChatContainer'
+
 
 const HomePage = () => {
+
+  const [selectedUser, setselectedUser] = useState(false);
+
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-    
+    <div className={`backdrop-blur-x1 border-2 border-gray-600 rounded-2x1 overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}
+    `}>
+      <Sidebar />
+      <ChatContainer />
+      <RightSidebar />
+    </div>
     </div>
   )
 }
