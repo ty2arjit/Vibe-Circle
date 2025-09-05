a6c9a573b2fa9e3f7c3059dc73b7c4850bc053e1
ty2arjit <ty2arjit@gmail.com>
2025-09-05 20:36:38 +0530

    background image added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,10 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
+import bgImage from './assets/bgImage.svg'
 
 const App = () => {
   return (
-    <div className="bg-[url('./src/assests/bgImage.svg')] bg-contain">
+    <div 
+      className="min-h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
